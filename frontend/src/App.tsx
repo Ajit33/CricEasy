e586ff33226f9e92d2d1a9be1dad16fbafae8360
Ajit33 <justajit33@gmail.com>
2025-01-11 00:49:54 +0530

    frontend init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+
+  return (
+  <div>
+    <h1 className='text-rose-400'>CircEasy makes your effort easy..</h1>
+    <Button>Get strated</Button>
+  </div>
+  )
+}
+
+export default App
